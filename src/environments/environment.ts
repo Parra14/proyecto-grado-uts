38008e04355fe41a5080de18d3ce0eab408ba1ae
Parra14 <91274355+Parra14@users.noreply.github.com>
2022-10-18 23:15:49 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiURL: 'http://localhost:4000/api',
+  production: true,
+  apiURL: 'http://localhost:4000/api'
+
 };
 
 /*
